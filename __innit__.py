82fcb391e67fcd3f88d8ea0e67ff1258b0fd3755
Alfa <m.alfaxx08@gmail.com>
2025-06-20 09:01:59 +0700

    Add __innit__.py file.

---
+++ __innit__.py
@@ -0,0 +1,4 @@
+from .pronounce import pronounce #type: ignore
+from .util import util #type: ignore
+from .Inversion import inversion #type: ignore
+from .Optimization import optimization #type: ignore
